4baa8f213ac24c5a993adabb6b8729958a24150d
prashantsnehi <prashant.snehi@gmail.com>
2024-02-03 15:13:00 +0530

    Routing

--- Program.cs
+++ Program.cs
@@ -1,8 +1,70 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddRouting(options =>
+    //options.ConstraintMap.Add("months", typeof(MonthCustomConstraint)));
+
 builder.Services.AddControllers(); // adds all the controller classes as services
 
 var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+
 app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/employee/profile/{employeename:minlength(3)=prashant}", async (context) =>
+    {
+        string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+
+        if (string.IsNullOrEmpty(employeeName))
+        {
+            await context?.Response?.WriteAsync("No value supplied")!;
+        }
+        else
+        {
+            await context.Response.WriteAsync($"Employee Name: {employeeName}");
+        }
+    });
+
+    endpoints.MapGet("/student/profile/{studentname:length(3, 10):alpha=prashant}", async (context) =>
+    {
+        string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+
+        if (string.IsNullOrEmpty(employeeName))
+        {
+            await context?.Response?.WriteAsync("No value supplied")!;
+        }
+        else
+        {
+            await context.Response.WriteAsync($"Employee Name: {employeeName}");
+        }
+    });
+
+    endpoints.Map("sales-report/{year:int:min(1900)}/{month:regex(^(apr|jul|oct|jan)$)}", async context =>
+    {
+        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+        string? month = textInfo.ToTitleCase(Convert.ToString(context.Request.RouteValues["month"]));
+        await context.Response.WriteAsync($"<h1>Sales Report for - {month}-{year}</h1>");
+    });
+
+    endpoints.Map("financial-years/{month:months}", async context =>
+    {
+        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        string? month = textInfo.ToTitleCase(Convert.ToString(context.Request.RouteValues["month"]));
+        await context.Response.WriteAsync($"<h1>Month: {month}</h1>");
+
+    });
+});
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync($"<h1>No route found for path: {context.Request.Path}</h1>");
+});
 
 app.Run();
 
